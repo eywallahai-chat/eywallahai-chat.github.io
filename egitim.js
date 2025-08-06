9331188045f9bc67f6d0984dce4c69683d64e0db
eywallahai-chat <eywallahai@gmail.com>
2025-08-06 19:54:12 +0300

    Create egitim.js

---
+++ egitim.js
@@ -0,0 +1,9 @@
+export const egitim = {
+  projeTanimi: "Eywallah AI, kullanıcı dostu...",
+  hedefOzellikler: [
+    "Anlayışlı ve empatik iletişim",
+    "Gelişmiş Türkçe dil yeteneği",
+    // ...
+  ],
+  // Devamı...
+};
