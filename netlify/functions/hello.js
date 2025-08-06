cd667ba2011c74b7a58ce72f9b2b944060920828
eywallahai-chat <eywallahai@gmail.com>
2025-08-06 19:34:57 +0300

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,1 +1,9 @@
-
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Eywallah, Netlify Function çalıştı!",
+      timestamp: new Date().toISOString()
+    }),
+  };
+};
